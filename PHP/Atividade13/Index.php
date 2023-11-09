4aba79091bef21ba8814f16d602a18def9114a6a
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2023-11-08 23:23:24 -0300

    Update Index.php

--- PHP/Atividade13/Index.php
+++ PHP/Atividade13/Index.php
@@ -2,18 +2,18 @@
     require_once "model\\pessoa.php";
 
     $pessoa = new \app\model\pessoa();
-    $pessoa->setNome("Gustavo");
-    $pessoa->setSobreNome("Verdi");
+    $pessoa->setNome("Rafael");
+    $pessoa->setSobreNome("Munarin");
     $pessoa->setDataNascimento(new DateTime("2002-01-08"));
 
     $pessoa->getTelefone()->setTipo(1);
     $pessoa->getTelefone()->setNome("Tel Celular");
-    $pessoa->getTelefone()->setValor("(47) 99970-7572");
+    $pessoa->getTelefone()->setValor("(47) 99675-9690");
 
-    $pessoa->getEndereco()->setLogradouro("Rua 7 de setembro");
-    $pessoa->getEndereco()->setBairro("Brehmer");
+    $pessoa->getEndereco()->setLogradouro("Rua Regina Pasqualini");
+    $pessoa->getEndereco()->setBairro("Canta Galo");
     $pessoa->getEndereco()->setCidade("Rio do Sul");
-    $pessoa->getEndereco()->setCEP("89160-000");
+    $pessoa->getEndereco()->setCEP("89163264");
     $pessoa->getEndereco()->setEstado("SC");
 
     echo $pessoa->toJson();
